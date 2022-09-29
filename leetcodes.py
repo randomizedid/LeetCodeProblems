adcd94cdbbcbf1ad8ace9886ce4548b7c3e8eebb
FrancescoB <frebonna@gmail.com>
2022-09-29 19:41:15 +0200

    added problem

--- leetcodes.py
+++ leetcodes.py
@@ -121,7 +121,23 @@ def MaximumSubarray(nums):
     return ans
 
 # Roman to Integer (runtime error)
-def romanToInteger(s):
+def romanToIntegerOne(s):
+    newdic = {'I':1, 'V':5, 'X':10, 'L':50, 'C':100, 'D':500, 'M':1000, 'IV':4}
+    sum = 0
+    for i in range(len(s)-1):
+        if s[i] == 'I' and s[i+1]=='V' or s[i] == 'I' and s[i+1]=='X':
+            sum-=1
+        elif s[i] == 'X' and s[i+1]=='L' or s[i] == 'X' and s[i+1]=='C':
+            sum-=10
+        elif s[i] == 'C' and s[i+1]=='D' or s[i] == 'C' and s[i+1]=='M':
+            sum-=100
+        else:
+            sum+=newdic[s[i]]
+    sum+=newdic[s[i+1]]
+    return sum
+
+# Roman to Integer (98ms and 14 MB)
+def romanToIntegerTwo(s):
     newdic = {'I':1, 'V':5, 'X':10, 'L':50, 'C':100, 'D':500, 'M':1000, 'IV':4, 'IX':9, 'XL':40, 'XC':90, 'CD':400, 'CM':900}
     sum = i = 0
     while i<len(s)-1:
@@ -134,4 +150,4 @@ def romanToInteger(s):
     return sum
 
 s = "MCMXCIV"
-print(romanToInteger(s))
+print(romanToIntegerTwo(s))
